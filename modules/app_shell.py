829de64a42ada85a5db32e31536aba6c981954b8
Abneriii <1444170707@qq.com>
2026-04-12 20:21:24 +0800

    版本号更新

--- modules/app_shell.py
+++ modules/app_shell.py
@@ -36,7 +36,7 @@ APP_DIR = RUNTIME_PATHS.app_root
 BASE_DATA_DIR = RUNTIME_PATHS.base_data_root
 
 APP_NAME = '纸研社'
-APP_VERSION = 'v1.2.6'
+APP_VERSION = 'v1.2.8'
 STARTUP_REG_PATH = r'Software\Microsoft\Windows\CurrentVersion\Run'
 STARTUP_VALUE_NAME = APP_NAME
 TOP_NAV_ITEMS = (
